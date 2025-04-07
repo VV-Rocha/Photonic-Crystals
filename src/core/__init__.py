17dc764b005fa35d8a72ee92d6910ec7a991b6f6
VV-Rocha <vicentevrocha.vvr@gmail.com>
2025-04-07 16:45:13 +0100

    Updated the __init__.py of core to expose the beams, control, dimensionless, eq_coefs, media and mesh.  Also, the folders are identified in __all__ list.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,3 +1,18 @@
-from .dimensionless import WavevectorScale
+__all__ = ['beams',
+           'control',
+           'dimensionless',
+           'eq_coefs',
+           'media',
+           'mesh']
 
-from .mesh import Mesh2D
+from . import beams
+
+from . import control
+
+from . import dimensionless
+
+from . import eq_coefs
+
+from . import media
+
+from . import mesh

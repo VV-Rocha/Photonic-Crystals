f23237e297a22279cd9b200d23c4030f2d5dd0ac
VV-Rocha <vicentevrocha.vvr@gmail.com>
2025-04-24 11:36:04 +0100

    Altered the coupled split step solver:     - The sign of the linear step was inverted to agree with new convention where the minus sign making the evolution forward in time is absorbed by the kinetic coefficient.     - The absorption operator has the minus sign making the exponential decaying in time for alpha positive. The absorption coefficient is defined as alpha/2.     - The nonlinear term was changed to account for the changes of the potential field functions. The potential field functions were altered to accept Field object instead of intensity field. The split step solver now passes the Field object instead of the intensity field.
    The split_step_function was altered to accept the Fields object instead of the field arrays.

--- src/simulators/nls_2d/coupled_eqs/split_step/split_step.py
+++ src/simulators/nls_2d/coupled_eqs/split_step/split_step.py
@@ -12,9 +12,9 @@ def linear_step(field, kinetic, mesh, np_complex, af_complex):
     """
     field[:,:] = af.signal.fft2(field)
     
-    exp = af.exp(-(1j * .5*mesh.mesh.dz * (mesh.kXX**2 + mesh.kYY**2) * kinetic))
+    exp = af.exp((1j * .5*mesh.mesh.dz * (mesh.kXX**2 + mesh.kYY**2) * kinetic))  # minus sign is absorbed in the kinetic coefficient
     
-    field[:,:] = field * exp
+    field[:,:] = exp * field
     field[:,:] = af.signal.ifft2(field)
     
 def absorption(field, absorption, dz, np_float, af_complex):
@@ -27,10 +27,10 @@ def absorption(field, absorption, dz, np_float, af_complex):
         np_float (np.float): _description_
         af_complex (af.Dtype.c): _description_
     """
-    exp = af.exp(af.constant(-np_float(absorption*dz), 1, 1, dtype=af_complex))
+    exp = af.exp(af.constant(-np_float(absorption*dz), 1, 1, dtype=af_complex))  # absorption is defined as alpha/2
     field[:, :] = field * exp
     
-def nonlinear_step(field_a, field_b, coefs, dz, af_complex):
+def nonlinear_step(fields, coefs, dz, af_complex):
     """Inplace implementation of the nonlinear step of the split-step Fourier method for the 2D NLSE.
 
     Args:
@@ -43,15 +43,13 @@ def nonlinear_step(field_a, field_b, coefs, dz, af_complex):
         Isat1 (float): _description_
         af_complex (af.Dtype.c): _description_
     """
-    field_intensity = af.constant(1.0, *field_a.dims(), dtype=af_complex)
-    field_intensity[:,:] *= af.abs(field_a)*af.abs(field_a) + af.abs(field_b)*af.abs(field_b)
     
     # nonlinear term
-    field_a[:, :] = field_a * af.exp(-1j*dz*coefs.potential_function(field_intensity))
-    field_b[:, :] = field_b * af.exp(-1j*dz*coefs.potential_function1(field_intensity))
+    fields.field[:, :] = af.exp(-1j*dz*coefs.potential_function(fields)) * fields.field
+    fields.field1[:, :] = af.exp(-1j*dz*coefs.potential_function1(fields)) * fields.field1 
     
 # % TODO: The solver should be adapted to accept an input_field object rather than the fields separately. The the first function or decorator should be constructed to handle the field pre-processing to the solving methods.
-def split_step_solver(field, field1, mesh, coefs, precision_control):
+def split_step_solver(fields, mesh, coefs, precision_control):
     """Inplace single step evolution of the 2D NLSE using the split-step Fourier method.
 
     Args:
@@ -62,13 +60,13 @@ def split_step_solver(field, field1, mesh, coefs, precision_control):
         coefs1 (NLSE Coefficients Object): _description_
         precision_control (PrecisionControl Object): _description_
     """
-    linear_step(field, coefs.kinetic, mesh, precision_control.np_complex, precision_control.af_complex)
-    linear_step(field1, coefs.kinetic1, mesh, precision_control.np_complex, precision_control.af_complex)
+    linear_step(fields.field, coefs.kinetic, mesh, precision_control.np_complex, precision_control.af_complex)
+    linear_step(fields.field1, coefs.kinetic1, mesh, precision_control.np_complex, precision_control.af_complex)
     
-    nonlinear_step(field, field1, coefs, mesh.mesh.dz, precision_control.af_complex)
+    nonlinear_step(fields, coefs, mesh.mesh.dz, precision_control.af_complex)
     
-    absorption(field, coefs.absorption, mesh.mesh.dz, precision_control.np_float, precision_control.af_complex)
-    absorption(field1, coefs.absorption1, mesh.mesh.dz, precision_control.np_float, precision_control.af_complex)
+    absorption(fields.field, coefs.absorption, mesh.mesh.dz, precision_control.np_float, precision_control.af_complex)
+    absorption(fields.field1, coefs.absorption1, mesh.mesh.dz, precision_control.np_float, precision_control.af_complex)
     
-    linear_step(field, coefs.kinetic, mesh, precision_control.np_complex, precision_control.af_complex)
-    linear_step(field1, coefs.kinetic1, mesh, precision_control.np_complex, precision_control.af_complex)
+    linear_step(fields.field, coefs.kinetic, mesh, precision_control.np_complex, precision_control.af_complex)
+    linear_step(fields.field1, coefs.kinetic1, mesh, precision_control.np_complex, precision_control.af_complex)

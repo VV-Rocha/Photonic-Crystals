27eec6827d826f32d8f948ba6d0e205c9e60d4b1
VV-Rocha <vicentevrocha.vvr@gmail.com>
2025-08-04 10:46:07 +0100

    Reorganised ordering of calls

--- src/simulators/simulation_box.py
+++ src/simulators/simulation_box.py
@@ -40,41 +40,25 @@ class SimulationBox(DataClass):
         self.medium_parameters = self.medium_config["object"](medium_config=self.medium_config)
         
         self.beam_parameters = self.beam_config["object"](beam_config=self.beam_config)
+            
+        self.modulation_properties = self.modulation_config["object"](
+            modulation_config = self.modulation_config
+            )
         
+        ### Define Simulation Box
+        ## Dimensionalisation
         self.adim_method = self.simulation_config["adim_method"](
             self.beam_parameters,
             self.medium_parameters,
             self.precision,
             )
-            
-        self.modulation_properties = self.modulation_config["object"](
-            modulation_config = self.modulation_config
-            )
-    
-        self.coefs = self.simulation_config["coefs_object"](
-            self.medium_parameters,
-            self.beam_parameters,
-            self.adim_method,
-            self.storage,
-            # invert_energy_scale=True,
-            )
-        
+        ## Grid
         self.mesh = self.simulation_config["mesh_method"](
             self.simulation_config,
             self.adim_method,
             )
-        
-        self.solver = self.simulation_config["solver"](
-            mesh=self.mesh,
-            coefs=self.coefs,
-            solver_method=self.simulation_config["solver_engine"],
-            precision_control=self.precision,
-            device = self.simulation_config["device"],
-            gpu_backend = self.simulation_config["backend"],
-            )
-        
+        ## Initial Conditions
         self.input_fields = self.simulation_config["fields"](
-            self.simulation_config,
             modulation_config=self.modulation_properties,
             precision_control=self.precision,
             store_config=self.storage,
@@ -82,6 +66,25 @@ class SimulationBox(DataClass):
         self.input_fields.gen_fields(
             self.mesh,
             self.simulation_config["noise"],
+            )
+        
+        ### Define Solver
+        ## Eq. coefficients
+        self.coefs = self.simulation_config["coefs_object"](
+            self.medium_parameters,
+            self.beam_parameters,
+            self.adim_method,
+            self.storage,
+            # invert_energy_scale=True,
+            )
+        ## engine
+        self.solver = self.simulation_config["solver"](
+            mesh=self.mesh,
+            coefs=self.coefs,
+            solver_method=self.simulation_config["solver_engine"],
+            precision_control=self.precision,
+            device = self.simulation_config["device"],
+            gpu_backend = self.simulation_config["backend"],
             )
                 
     def solve(self,):

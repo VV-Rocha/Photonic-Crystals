fdb7e0de97055e8301899145bd2e555ed963dc09
VV-Rocha <vicentevrocha.vvr@gmail.com>
2025-08-11 14:54:41 +0100

    Changes in photorefractive.py in folder src\core\media:     - Removed storage methods.     - Renamed the parameter medium_config to crystal_config.     - The way delta_n_max and delta_n_max1 were being calculated was changed to use a single function.

--- src/core/media/photorefractive.py
+++ src/core/media/photorefractive.py
@@ -1,49 +1,46 @@
-import h5py
+def delta_n_max(n, electro_optic_coef, tension, Lx):
+    return .5 * n**3 * electro_optic_coef * tension / Lx
 
-# % TODO: Add a function to store variables for the PhotorefractiveCrystal class.
-
-# % TODO: Change the classes to use the @dataclass method to ease readability and @classmethod to initiate delta_n_max.
-
-class PhotorefractiveCrystal:
+class PhotorefractiveCrystalParameters:
     """A class representing a photorefractive crystal parameters with a single incident beam."""
     def __init__(
         self,
-        medium_config,
+        crystal_config,
         ):
         """Initialize the photorefractive crystal when a single beam is being used.
 
         Args:
-            medium_config (dict): Dictionary with all the physical parameters required to initiate the object.
+            crystal_config (dict): Dictionary with all the physical parameters required to initiate the object.
         """
-        self.n = medium_config["n"]
-        self.electro_optic_coef = medium_config["electro_optic_coef"]
-        self.tension = medium_config["tension"]
-        self.Isat = medium_config["Isat"]
-        self.alpha = medium_config["alpha"]
+        self.n = crystal_config["n"]
+        self.electro_optic_coef = crystal_config["electro_optic_coef"]
+        self.tension = crystal_config["tension"]
+        self.Isat = crystal_config["Isat"]
+        self.alpha = crystal_config["alpha"]
         
-        self.Lx = medium_config["Lx"]
-        self.Ly = medium_config["Ly"]
-        self.Lz = medium_config["Lz"]
+        self.Lx = crystal_config["Lx"]
+        self.Ly = crystal_config["Ly"]
+        self.Lz = crystal_config["Lz"]
         
         # Computes and initializes the delta_n_max variable.
-        self.delta_n_max = .5 * self.n**3 * self.electro_optic_coef * self.tension / self.Lx
+        self.delta_n_max = delta_n_max(.5 * self.n**3 * self.electro_optic_coef * self.tension / self.Lx)
 
-class TwoBeamPhotorefractiveCrystal(PhotorefractiveCrystal):
+class SecondBeamPhotorefractiveCrystalParameters(PhotorefractiveCrystalParameters):
     """A class representing a photorefractive crystal parameteres for two incident beams."""
     def __init__(
         self,
-        medium_config,
+        crystal_config,
         ):
         """Initialize the photorefractive crystal when using two light beams.
 
         Args:
-            medium_config (dict): Dictionary with all the physical parameters required to initiate the object.
+            crystal_config (dict): Dictionary with all the physical parameters required to initiate the object.
         """
-        super().__init__(medium_config)
+        super().__init__(crystal_config)
         
-        self.n1 = medium_config["n1"]
-        self.electro_optic_coef1 = medium_config["electro_optic_coef1"]
-        self.alpha1 = medium_config["alpha1"]
+        self.n1 = crystal_config["n1"]
+        self.electro_optic_coef1 = crystal_config["electro_optic_coef1"]
+        self.alpha1 = crystal_config["alpha1"]
         
         # Computes and initializes the delta_n_max for the second beam.
-        self.delta_n_max1 = .5 * self.n1**3 * self.electro_optic_coef1 * self.tension / self.Lx
+        self.delta_n_max1 = delta_n_max(.5 * self.n1**3 * self.electro_optic_coef1 * self.tension / self.Lx)

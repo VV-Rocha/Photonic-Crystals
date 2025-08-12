35b28c5efed679738588135d9dc1c76e76c407c8
VV-Rocha <vicentevrocha.vvr@gmail.com>
2025-08-12 11:36:40 +0100

    Updated __init__.py to account for the move of nls_2d\ into nls\

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,9 @@
 __all__ = ['Solver',
            'SimulationBox',
-           'nls_2d',
+           'nls',
            ]
 
-from . import nls_2d
+from . import nls
 
 from .solver_wrapper import Solver
 from .simulation_box import SimulationBox

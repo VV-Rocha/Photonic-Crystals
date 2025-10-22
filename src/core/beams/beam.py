50bd2e6f83b956abba9a019b13b97e28d9758ff1
VV-Rocha <vicentevrocha.vvr@gmail.com>
2025-10-22 13:22:22 +0100

    - Prepared the Beam and TwoBeams to be inherited by higher-level objects

--- src/core/beams/beam.py
+++ src/core/beams/beam.py
@@ -1,14 +1,14 @@
-from functools import wraps
-
 from typing import Tuple
-
 from numpy import pi
 
 class Beam:
     """Beam class holding fundamental beam properties."""
-    def __init__(self,
-                 beam_config,
-                 ):
+    def __init__(
+        self,
+        beam_config,
+        *args,
+        **kwargs,
+        ):
         """Initialize the class holding the fundamental beam properties.
 
         Args:
@@ -19,12 +19,27 @@ class Beam:
         self.c = beam_config["c"]
         
         self.k = 2*pi/self.wavelength
+                
+        super().__init__(
+            beam_config = beam_config,
+            *args,
+            **kwargs,
+            )
+
+def second_value(key: str, beam_config: dict) -> float:
+    if key in beam_config.keys():
+        param = beam_config[key]
+    else:
+        param = beam_config[key[:-1]]
+    return param
 
 class TwoBeams(Beam):
     """Two beam class for holding fundamental beam properties."""
     def __init__(
         self,
-        beam_config,         
+        beam_config,
+        *args,
+        **kwargs,
         ):
         """Initializes the class holding the fundamental properties of two beams.
 
@@ -33,8 +48,13 @@ class TwoBeams(Beam):
             cs (Tuple[float, float]): Tuple with the coupling with the medium for each beam.
             store_config (StoreConfig, optional): Class object from control.storage_config. Defaults to None.
         """
-        super().__init__(beam_config=beam_config)
-
-        self.wavelength1 = beam_config["wavelength1"]
-        self.c1 = beam_config["c1"]
+        self.wavelength1 = second_value("wavelength1", beam_config)
+        self.c1 = second_value("c1", beam_config)
         
+        self.k1 = 2*pi/self.wavelength1
+        
+        super().__init__(
+            beam_config = beam_config,
+            *args,
+            **kwargs,
+            )

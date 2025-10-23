a738cbc495d46a25e7a89ac1aeb663bef1462341
VV-Rocha <vicentevrocha.vvr@gmail.com>
2025-10-23 15:13:38 +0100

    feat(core/engines/workbench): add Workbench1D and Workbench2D classes which are to be used as bases to build higher-level objects with analysis tools. For now, these feature the corresponding Mesh objects. Later developements can add other base methods.
    - Added `system_1d.py` and `system_2d.py` implementing `Workbench1D` and `Workbench2D`.
    - Each class inherits from corresponding mesh classes (`Mesh1D`, `Mesh2D`) and initializes the simulation mesh via `init_workbench()`.
    - Establishes a consistent interface for 1D and 2D workbench initialization.

---
+++ src/core/engines/workbench/system_1d.py
@@ -0,0 +1,5 @@
+from ...mesh.z_1d import Mesh1D
+
+class Workbench1D(Mesh1D):
+    def init_workbench(self,):
+        self.init_mesh()
---
+++ src/core/engines/workbench/system_2d.py
@@ -0,0 +1,5 @@
+from ...mesh.z_2d import Mesh2D
+
+class Workbench2D(Mesh2D):
+    def init_workbench(self,):
+        self.init_mesh()

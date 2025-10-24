4a4bfafcbe7ee1362a436fdefc8b0ddbf3eb916b
VV-Rocha <vicentevrocha.vvr@gmail.com>
2025-10-24 17:30:26 +0100

    refactor(core/fields/landscapes/lattices/moire_lattices/): simplify double_lattices implementation
    - Rewrote `double_lattices.py` with a single `lattice_sum()` function.
    - Merges two rotated lattice fields with configurable weights and normalization.
    - Improves readability and modularity for moiré lattice generation.

--- src/fields/landscapes/lattices/moire_lattices/double_lattices.py
+++ src/fields/landscapes/lattices/moire_lattices/double_lattices.py
@@ -1,58 +1,17 @@
-from ..single_lattices.two_dimensional import planewave_lattice
-from functools import wraps
+import numpy as np
 
-from typing import Tuple
-
-from numpy import max
-
-def check_tuple(func):
-    @wraps(func)
-    def wrapper(mesh, a, p, *args, **kwargs):
-        if type(a) is not tuple:
-            a = (a, a)
-        if type(p) is not tuple:
-            p = (p, p)
-        return func(mesh, a, p, *args, **kwargs)
-    return wrapper
-
-def join_lattices(mesh,
-                  a,
-                  p,
-                  rotation,
-                  base_lattice,
-                  ):
-    lattice1 = base_lattice[0](mesh, a[0], rotation[0])
-    lattice2 = base_lattice[1](mesh, a[1], rotation[1])
-    
-    lattice = (p[0]*lattice1 + p[1]*lattice2)/max(p[0]*lattice1 + p[1]*lattice2)
-    
-    return lattice
-    
-@check_tuple
-def double_lattice(mesh,
-                  a: Tuple[float, float],
-                  p: Tuple[float, float],
-                  rotation: Tuple[float, float],
-                  single_lattice=planewave_lattice,
-                  join_lattices=join_lattices,
-                  ):
-    """Calls the join_lattices function to create a two lattice Moiré lattice using the single lattice given in single_lattice.
-
-    Args:
-        mesh (_type_): Mesh2D object containing the domain mesh grid.
-        a (Tuple[float, float]): Lattice parameters for the two lattices.
-        p (Tuple[float, float]): Lattice weights for the two lattices.
-        rotation (Tuple[float, float]): Rotation angles of the lattices in radians.
-        single_lattice (func, optional): Single lattice function. Defaults to planewave_lattice.
-        join_lattices (func, optional): Function to join two lattices. Defaults to join_lattices.
-
-    Returns:
-        ndarray: Moiré lattice.
-    """
-    lattice = join_lattices(mesh,
-                            a,
-                            p,
-                            rotation,
-                            single_lattice,
-                            )
-    return lattice
+def lattice_sum(
+    xx_rot0,
+    yy_rot0,
+    xx_rot1,
+    yy_rot1,
+    a,
+    p,
+    lattice_function
+    ):
+    l_ = p[0] * lattice_function(xx_rot0, yy_rot0, a)
+    l_ += p[1] * lattice_function(xx_rot1, yy_rot1, a)
+        
+    l_ /= np.max(np.abs(l_))
+        
+    return l_

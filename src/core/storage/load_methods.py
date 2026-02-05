21692967e5d22de189f9a9a030a189db3ce51ebf
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-02-05 16:11:24 +0000

    Changes to LoadField. Changed load_field to get_field. The previous version loaded the field onto an internal variable. The new method returns the loaded field without storing internally.
    Changes to LoadSimulation. Added a method get_last_field and get_input_field which return the last and first field of the workspace directory given, respectively. Added a mount_field which loads a field with a given index onto the internal variable fields. Added a load_field which loads the entire simulation, based on store variable being either "last" or "stride" this method loads either the first and last fields or the full history.
    
    Added LoadCoupledSimulation which extends the previous class to load field1.

--- src/core/storage/load_methods.py
+++ src/core/storage/load_methods.py
@@ -1,118 +1,114 @@
 import h5py
 import numpy as np
 
+from .directories import FieldDirectories
+from .directories import CoupledFieldDirectories
 
-class LoadField:
+class LoadField(FieldDirectories):
     """ Class to load simulation fields from storage."""
-    def load_field(
+    def get_field(
         self,
-        index: str | int | None = None,
+        directory: str,
         ):
-        """ Load the field from storage.
+        """ Get the field from directory.
 
         Args:
-            index (str | int | None, optional): Index of the field to load. Can be "last" or an integer index. Defaults to None.
+            directory (str): Directory of the field .h5 file.
         """
-        with h5py.File(self.get_field_directory(index), "r") as f:
-            self.field = f["field"][:]
+        with h5py.File(directory, "r") as f:
+            field = f["field"][:]
         f.close()
-        
+        return field
+
 class LoadSimulation(LoadField):
     """ Class to load entire simulation fields from storage."""
-    def init_fields_zeros(self,):
-        """ Initialize the fields array with zeros based on storage mode."""
-        if self.store.lower() == "last":
-            self.fields = np.zeros((2, *self.field_shape), dtype=np.complex128)
-        else:
-            self.fields = np.zeros((self.Nsteps, *self.field_shape), dtype=np.complex128)
-    
-    def load_last_field(self,):
+    def get_last_field(self,):
         """ Load the last stored field into the fields array."""
-        self.fields[1] = self.load_field()
+        return self.get_field(self.get_field_directory("last"))
+    
+    def get_input_field(self,):
+        return self.get_field(self.get_field_directory(0))
     
     def mount_field(self, index = None):
         """ Load a specific field and mount it into the fields array."""
-        self.load_field(index)
-        self.fields[index] = self.field
+        self.fields[index] = self.get_field(self.get_field_directory(index))
     
-    def load_simulation(self,):
+    def load_field(self,):
         """ Load the entire simulation fields from storage."""
-        self.init_fields_zeros()
-
-        self.mount_field(index=0) # load initial state
-        
         if self.store.lower() == "last":
-            self.load_last_field()
+            self.fields = np.zeros((2, *self.field_shape), dtype=np.complex128)
+            self.fields[0] = self.get_input_field()
+            self.fields[1] = self.get_last_field()
         else:
-            for i in range(1, self.Nsteps):
+            self.fields = np.zeros((self.Nsteps+1, *self.field_shape), dtype=np.complex128)
+            for i in range(self.Nsteps + 1):
                 self.mount_field(index = i)
                 
-class CoupledLoadField:
-    """ Class to load coupled simulation fields from storage."""
-    def load_field(
-        self,
-        directory,
-        ):
-        """ Load the first field from storage."""
-        with h5py.File(directory, "r") as f:
-            self.field = f["field"][:]
-        f.close()
-        
-    def load_field1(
-        self,
-        directory,
-        ):
-        """ Load the second field from storage."""
-        with h5py.File(directory, "r") as f:
-            self.field1 = f["field"][:]
-        f.close()
-
-class LoadCoupledSimulation(CoupledLoadField):
+class LoadCoupledSimulation(CoupledFieldDirectories, LoadSimulation):
     """ Class to load entire coupled simulation fields from storage."""
-    def init_fields_zeros(self, field_number=0):
-        """ Initialize the coupled fields arrays with zeros based on storage mode."""
-        if field_number is None:        
-            if self.store.lower() == "last":
-                self.fields = np.zeros((2, *self.field_shape), dtype=np.complex128)
-                self.fields1 = np.zeros((2, *self.field_shape), dtype=np.complex128)
-            else:
-                self.fields = np.zeros((self.Nsteps, *self.field_shape), dtype=np.complex128)
-                self.fields1 = np.zeros((self.Nsteps, *self.field_shape), dtype=np.complex128)
-        elif field_number is 0:
-            if self.store.lower() == "last":
-                self.fields = np.zeros((2, *self.field_shape), dtype=np.complex128)
-            else:
-                self.fields = np.zeros((self.Nsteps, *self.field_shape), dtype=np.complex128)
-        elif field_number is 1:
-            if self.store.lower() == "last":
-                self.fields1 = np.zeros((2, *self.field_shape), dtype=np.complex128)
-            else:
-                self.fields1 = np.zeros((self.Nsteps, *self.field_shape), dtype=np.complex128)
+    def get_last_field(self,):
+        """ Load the last stored field into the fields array."""
+        return self.get_field(self.get_field_directory("last")[0])
+    
+    def get_input_field(self,):
+        return self.get_field(self.get_field_directory(0)[0])
+    
+    def mount_field(self, index = None):
+        """ Load a specific field and mount it into the fields array."""
+        self.fields[index] = self.get_field(self.get_field_directory(index)[0])
+    
+    def mount_field1(self, index = None):
+        """ Load a specific field and mount it into the fields array."""
+        self.fields1[index] = self.get_field(self.get_field_directory(index)[1])
+    
+    def mount_fields(self, index = None):
+        """ Mount fields."""
+        self.fields[index] = self.get_field(self.get_field_directory(index)[0])
+        self.fields1[index] = self.get_field(self.get_field_directory(index)[1])
+    
+    def get_input_field1(self,):
+        return self.get_field(self.get_field_directory(0)[1])
+    
+    def get_last_field1(self,):
+        """ Load the last stored field into the fields array."""
+        return self.get_field(self.get_field_directory("last")[1])
 
-    def load_last_field(self,):
-        """ Load the last stored coupled fields into the fields arrays."""
-        directory = self.get_field_directory("last")
-        self.load_field(directory[0])
-        self.fields[1] = self.field
-
-    def load_simulation(self, field_number=0):
-        """ Load the entire coupled simulation fields from storage.
-
-        Args:
-            field_number (int, optional): field number to load (0, 1 or None for both). Defaults to 0.
-        """
-        self.init_fields_zeros(field_number)
+    def load_field(self,):
+        """ Load the entire simulation fields from storage."""
+        if self.store.lower() == "last":
+            self.fields = np.zeros((2, *self.field_shape), dtype=np.complex128)
+            self.fields[0] = self.get_input_field()
+            self.fields[1] = self.get_last_field()
+        else:
+            self.fields = np.zeros((self.Nsteps+1, *self.field_shape), dtype=np.complex128)
+            for i in range(self.Nsteps + 1):
+                self.mount_field(index = i)
+    
+    def load_field1(self,):
+        """ Load the entire simulation fields from storage."""
+        if self.store.lower() == "last":
+            self.fields1 = np.zeros((2, *self.field_shape), dtype=np.complex128)
+            self.fields1[0] = self.get_input_field1()
+            self.fields1[1] = self.get_last_field1()
+        else:
+            self.fields1 = np.zeros((self.Nsteps+1, *self.field_shape), dtype=np.complex128)
+            for i in range(self.Nsteps + 1):
+                self.mount_field1(index = i)
         
-        if field_number is 0:
-            # load initial state
-            directory = self.get_field_directory(0)
-            self.load_field(directory[0])
-            self.fields[0] = self.field
-        
+    def load_fields(
+        self,
+        field_number = None,
+    ):
+        if field_number == 0:
+            self.load_field()
+        elif field_number == 1:
+            self.load_field1()
+        else:
             if self.store.lower() == "last":
-                self.load_last_field()
-            else:
-                for i in range(1, self.Nsteps):
-                    directory = self.get_field_directory(i)
-                    self.load_field(directory[0])
-                    self.fields[i] = self.field
+                self.load_field()
+                self.load_field1()
+            elif self.store.lower() == "stride":
+                self.fields = np.zeros((self.Nsteps+1, *self.field_shape), dtype=np.complex128)
+                self.fields1 = np.zeros((self.Nsteps+1, *self.field_shape), dtype=np.complex128)
+                for i in range(self.Nsteps + 1):
+                    self.mount_fields(index = i)

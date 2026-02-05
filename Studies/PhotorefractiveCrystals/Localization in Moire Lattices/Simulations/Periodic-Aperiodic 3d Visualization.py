7844d851d36b0647676f47776ddc03b135406dda
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-02-05 15:48:47 +0000

    Changed 3D plotting to work as an internal method of the object.
    Standardizes the 3D plotting pipeline.

--- Studies/PhotorefractiveCrystals/Localization in Moire Lattices/Simulations/Periodic-Aperiodic 3d Visualization.py
+++ Studies/PhotorefractiveCrystals/Localization in Moire Lattices/Simulations/Periodic-Aperiodic 3d Visualization.py
@@ -11,33 +11,35 @@ from src.core.boxes.simulation import SimulationBoxMethods
 
 from src.core.engines.solvers.nls.eq_coefs.models import CoupledWavevectorPhotorefractiveModel
 
-from src.core.engines.solvers.nls.coupled_solver_2d.split_step.step_solver import CoupledSplitStepSolver
+from src.core.engines.solvers.nls.solver_2d.coupled_solver import CoupledSplitStepSolver
 
 from src.fields.fields_2d import SecondMoireLatticeGaussian2D
-from src.fields.plotting.field_2d import PlotCoupledFields
+from src.fields.plotting.field_2d import PlotCoupledFields2D
+from src.fields.plotting.field_3d import PlotCoupledFields3D
 
 
 inheritance = {
-    PlotCoupledFields,
+    PlotCoupledFields3D,
+    PlotCoupledFields2D,
     CoupledSplitStepSolver,
     SecondMoireLatticeGaussian2D,
     CoupledWavevectorPhotorefractiveModel,
 }
 
 periodic_storage_config = {"home": "./Data/Periodic/",
-                  "store": "last",
+                  "store": "stride",
                   "extension": ".h5",
                   "stride": 1
                   }
 
 aperiodic_storage_config = {"home": "./Data/Aperiodic/",
-                            "store": "last",
+                            "store": "stride",
                             "extension": ".h5",
                             "stride": 1,
                             }
 
 free_storage_config = {"home": "./Data/Free/",
-                            "store": "last",
+                            "store": "stride",
                             "extension": ".h5",
                             "stride": 1,
                             }
@@ -50,7 +52,7 @@ simulation_config = {"Nx": 2*1024,
                      "lz": 1*20e-3,
                      "noise": .05,
                      "device": 0,
-                     "backend": "0",
+                     "backend": "cuda",
                      }
 
 crystal_config = {"n": 2.36,
@@ -79,7 +81,7 @@ state_modulation_config = {"I": .3,
                            }
 
 lattice_modulation_config = {"I": (crystal_config["Isat"])*16.,
-                           "width": 600e-6,
+                           "width": 700e-6,
                            "center": (0,0),
                            "exponent": 4.,
                            }
@@ -135,9 +137,8 @@ device_config = {
     "backend": "0",#"cuda",
 }
 
-
 free_modulation_config = {"I": 0.,
-                           "width": 600e-6,
+                           "width": 700e-6,
                            "center": (0,0),
                            "exponent": 4.,
                            }
@@ -149,6 +150,10 @@ free_modulation_config = {
     "envelope1_config": free_modulation_config,
 }
 
+plot3d_config = {
+    "rcount": (256, 256)
+}
+
 class SimulationBox(*inheritance, SimulationBoxMethods):
     def __init__(
         self,
@@ -158,6 +163,7 @@ class SimulationBox(*inheritance, SimulationBoxMethods):
         device_config,
         modulation_config,
         storage_config,
+        plot3d_config,
     ):
         super().__init__(
             crystal_config = crystal_config,
@@ -166,6 +172,7 @@ class SimulationBox(*inheritance, SimulationBoxMethods):
             device_config = device_config,
             modulation_config = modulation_config,
             storage_config = storage_config,
+            plot3d_config = plot3d_config,
         )
         self.store_configs(
             crystal_config,
@@ -183,6 +190,7 @@ periodic_SimBox = SimulationBox(
     device_config = device_config,
     modulation_config = periodic_modulation_config,
     storage_config = periodic_storage_config,
+    plot3d_config = plot3d_config,
 )
 print(">>> Done!")
 aperiodic_SimBox = SimulationBox(
@@ -192,6 +200,7 @@ aperiodic_SimBox = SimulationBox(
     device_config = device_config,
     modulation_config = aperiodic_modulation_config,
     storage_config = aperiodic_storage_config,
+    plot3d_config = plot3d_config,
 )
 print(">>> Done!")
 free_SimBox = SimulationBox(
@@ -201,6 +210,7 @@ free_SimBox = SimulationBox(
     device_config = device_config,
     modulation_config = free_modulation_config,
     storage_config = free_storage_config,
+    plot3d_config = plot3d_config,
 )
 print(">>> Done!")
 periodic_SimBox.init()
@@ -234,71 +244,6 @@ fig.savefig("./aperiodic_output.png", dpi=300)
 vmin = np.min([np.abs(periodic_SimBox.field)**2, np.abs(aperiodic_SimBox.field)**2])
 vmax = np.max([np.abs(periodic_SimBox.field)**2, np.abs(aperiodic_SimBox.field)**2])
 
-l = periodic_SimBox.adimensionalize_length(.15e-3)
-x_indices = np.where((periodic_SimBox.x>=-l) * (periodic_SimBox.x<=l))[0]
-y_indices = np.where((periodic_SimBox.y>=-l) * (periodic_SimBox.y<=l))[0]
-xx_indices, yy_indices = np.meshgrid(x_indices, y_indices)
-
-print("xmin, xmax =", periodic_SimBox.x.min(), periodic_SimBox.x.max())
-
-
-# from matplotlib.colors import Normalize, LogNorm
-
-def plot_3d(simbox, string):
-    fig = plt.figure()
-    axs = fig.add_subplot(111, projection='3d')  # <-- 3D axis
-    print(np.min(np.abs(simbox.field[xx_indices, yy_indices])**2))
-    print(np.max(np.abs(simbox.field[xx_indices, yy_indices])**2))
-    print("vmin, vmax =", vmin, vmax)
-    surf = axs.plot_surface(10**3 *simbox.dimensionalize_length(simbox.x[xx_indices]),
-                               10**3 *simbox.dimensionalize_length(simbox.y[yy_indices]),
-                               np.abs(simbox.field[xx_indices, yy_indices])**2,
-                               linewidth = 0,
-                               antialiased = False,
-                               cmap = "turbo",
-                               alpha = .8,
-                            #    rcount=rccount[0],     # draw every row
-                            #    ccount=rccount[1],     # draw every column
-                               vmin=vmin,
-                               vmax=vmax,
-                               )
-
-    fig.colorbar(surf,
-                     shrink=0.5,
-                     aspect=10,
-                     label=r'$Intensity (mW\cdot cm^{-2})$',
-                     )
-
-    axs.set_zlim(0., vmax)
-        
-    # Label axes
-    axs.set_xlabel('x (mm)')
-    axs.set_ylabel('y (mm)')
-    axs.set_zlabel('Intensity')
-
-    axs.view_init(elev=90, azim=45, roll=15)
-
-    fig.savefig(string, dpi=300, transparent=True)
-    
-def plot_2d(simbox, string):
-    fig, axs = plt.subplots(1)
-    axs.imshow(np.abs(simbox.field[xx_indices, yy_indices])**2, extent=[-l,l,-l,l])#, vmin=vmin, vmax=vmax)
-
-    # Label axes
-    axs.set_xlabel('X')
-    axs.set_ylabel('Y')
-    
-    fig.savefig(string, dpi=300)
-
-plot_2d(periodic_SimBox, "periodic_plot_2d.png")
-plot_2d(aperiodic_SimBox, "aperiodic_plot_2d.png")
-plot_2d(free_SimBox, "free_plot_2d.png")
-
-plot_3d(periodic_SimBox, "periodic_plot_3d.png")
-plot_3d(aperiodic_SimBox, "aperiodic_plot_3d.png")
-plot_3d(free_SimBox, "free_plot_3d.png")
-
-print("Aperiodic:", aperiodic_SimBox.get_intensity1().max(), "Periodic:", periodic_SimBox.get_intensity1().max(), "Free:", free_SimBox.get_intensity1().max(), )
 
 vmin = np.min([
     periodic_SimBox.get_intensity(),
@@ -332,6 +277,21 @@ periodic_SimBox.set_scale(scale="milimeter")
 aperiodic_SimBox.set_scale(scale="milimeter")
 free_SimBox.set_scale(scale="milimeter")
 
-periodic_SimBox.plot_2d_coupled(alpha=1, filename="periodic_coupled_fields")
-aperiodic_SimBox.plot_2d_coupled(alpha=1, filename="aperiodic_coupled_fields")
-free_SimBox.plot_2d_coupled(alpha=1, filename="free_coupled_fields")
+extent = .15e-3
+extent = [(-1)**(i+1) *extent for i in range(4)]
+periodic_SimBox.set_extent(extent)
+aperiodic_SimBox.set_extent(extent)
+free_SimBox.set_extent(extent)
+
+
+periodic_SimBox.plot_2d_coupled(alpha=.5, filename="periodic_coupled_fields")
+aperiodic_SimBox.plot_2d_coupled(alpha=.5, filename="aperiodic_coupled_fields")
+free_SimBox.plot_2d_coupled(alpha=.5, filename="free_coupled_fields")
+
+# periodic_SimBox.set_vlims(vmin, vmax)
+# aperiodic_SimBox.set_vlims(vmin, vmax)
+# free_SimBox.set_vlims(vmin, vmax)
+
+periodic_SimBox.plot3d_field(filename="periodic_3d")
+aperiodic_SimBox.plot3d_field(filename="aperiodic_3d")
+free_SimBox.plot3d_field(filename="free_3d")

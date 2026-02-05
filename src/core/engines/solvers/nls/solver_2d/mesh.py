26bc6b48e1c18129171149b1722a7bea3da0b03b
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-02-05 15:57:33 +0000

    Updated the SplitStepMesh by segmenting in two classes. The first class SplitStepMesh deals with the conversion of the required arrays between numpy and Arrayfire for the single field propagation case. The second CoupledSplitStepMesh extends the first to perform the same kind of conversion for the second field.

--- src/core/engines/solvers/nls/solver_2d/mesh.py
+++ src/core/engines/solvers/nls/solver_2d/mesh.py
@@ -1,17 +1,25 @@
-from ......mesh.z_2d import Mesh2D
+from .....mesh.z_2d import Mesh2D
 
 class SplitStepMesh(Mesh2D):
     def init_af(self,):
         self.field = self.np_to_af(self.field)
-        self.field1 = self.np_to_af(self.field1)
-        
+
         self.init_k_grid()
         self.kxx = self.np_to_af(self.kxx)
         self.kyy = self.np_to_af(self.kyy)
         
     def end_af(self,):
         self.field = self.af_to_np(self.field)
-        self.field1 = self.af_to_np(self.field1)
         
         self.kxx = self.af_to_np(self.kxx)
         self.kyy = self.af_to_np(self.kyy)
+
+
+class CoupledSplitStepMesh(SplitStepMesh):
+    def init_af(self,):
+        super().init_af()
+        self.field1 = self.np_to_af(self.field1)
+        
+    def end_af(self,):
+        super().end_af()
+        self.field1 = self.af_to_np(self.field1)

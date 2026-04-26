a7eb67d2d5fca2fd1978fdf5d61791572824e131
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-04-26 02:28:06 +0100

    Added UniformConfig to the uniform background envelope modulations.

---
+++ src/fields/backgrounds/uniform/uniform_config.py
@@ -0,0 +1,12 @@
+class UniformConfig:
+    def __init__(
+        self,
+        envelope_config,
+        *args,
+        **kwargs
+    ):
+        self.I = envelope_config["I"]
+        super().__init__(*args, **kwargs)
+    
+    def adimensionalize_envelope(self,):
+        pass

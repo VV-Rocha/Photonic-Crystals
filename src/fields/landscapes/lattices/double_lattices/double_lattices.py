05a577c2ec5fe314ef85e0b3b81db26d2b8ef1d8
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-05-07 21:50:54 +0100

    Changed name of folder moire_lattices to double_lattices.

---
+++ src/fields/landscapes/lattices/double_lattices/double_lattices.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def lattice_sum(
+    xx_rot0,
+    yy_rot0,
+    xx_rot1,
+    yy_rot1,
+    a,
+    p,
+    lattice_function
+    ):
+    l_ = p[0] * lattice_function(xx_rot0, yy_rot0, a)
+    l_ += p[1] * lattice_function(xx_rot1, yy_rot1, a)
+        
+    l_ /= np.max(np.abs(l_))
+    
+    return l_

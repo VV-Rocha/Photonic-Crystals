e648563af40398797856be150daa15d2336fc16e
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-05-08 15:35:35 +0100

    __init__.py added to the fields/ folder to expose the MoireLatticeGaussian, Gaussian, ShiqiXiaUniformSSH, and SiteBySiteUniformSSH modulations for the beams.

---
+++ src/fields/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'Gaussian',
+    'ShiqiXiaUniformSSH',
+    'SiteBySiteUniformSSH',
+    'MoireLatticeGaussian',
+]
+
+from .field_2d import MoireLatticeGaussian
+from .field_2d import Gaussian
+
+from .field_2d import ShiqiXiaUniformSSH, SiteBySiteUniformSSH

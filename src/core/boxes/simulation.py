2d4d865d107747730ac34bec232f1bfb0c2f64f8
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-05-08 13:49:28 +0100

    Eliminated deprecated SimulationBoxMethods which used inheritance of methods.
    Created a new AnalogousTime2DSimulationBox for simulation of light propagation in paraxial regimes where the longitudinal is mapped into an effective time.

--- src/core/boxes/simulation.py
+++ src/core/boxes/simulation.py
@@ -1,23 +1,37 @@
-from ..dimensionless.facade import DimensionlessMethods
+from .store_config.store import _store_config
 
-default_inheritances = (
-    DimensionlessMethods,
-    )
 
-class SimulationBoxMethods(*default_inheritances):
-    def __init__(self, *args, **kwargs):        
-        super().__init__(*args, **kwargs)
-        self.init()
+class AnalogousTime2DSimulationBox:
+    def __init__(
+        self,
+        beams: dict,
+        media: object,
+        model: object,
+        mesh: object,
+        solver: object,
+        storage: object,
+    ):
+        self.mesh = mesh
+        self.beams = beams
+        self.media = media
+        self.model = model
+        self.solver = solver
+        self.storage = storage
     
-    def init(self,):
-        self.init_model()
-        self.init_solver()        
-
-        self.adimensionalize_field()
-
-        self.modulate_field()
-        self.add_noise()
-        self.store_field(index="0")  # store initial state
-        if hasattr(self, "plot_flag"):
-            if self.plot_flag:
-                self.init_plot()
+    def init(self, *args, **kwargs):
+        self.store_config()
+        
+        self.mesh.init()
+        self.model.init(self, *args, **kwargs)
+        self.init_beams()
+        self.solver.init(self)
+    
+    def init_beams(self,):
+        for beam in self.beams.values():
+            beam.init(self)
+    
+    def solve(self,):
+        self.solver.solve(self)
+        
+    def store_config(self,):
+        _store_config(self.storage.config, vars(self))

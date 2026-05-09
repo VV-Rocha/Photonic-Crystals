4e5c62f41f99085aea2ab656a32fda34228d718a
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-05-09 22:47:17 +0100

    Exposure of the PhaseStepGaussian and the ContinuousFeatureGaussian beam modulations.

--- src/fields/__init__.py
+++ src/fields/__init__.py
@@ -1,11 +1,23 @@
 __all__ = [
+    # physics
     'Gaussian',
     'ShiqiXiaUniformSSH',
     'SiteBySiteUniformSSH',
     'MoireLatticeGaussian',
+    'PhaseStepGaussian',
+    # oc
+    'ContinuousFeatureGaussian',
 ]
 
+
+### Quantum Fluids
 from .field_2d import MoireLatticeGaussian
 from .field_2d import Gaussian
 
 from .field_2d import ShiqiXiaUniformSSH, SiteBySiteUniformSSH
+
+from .field_2d import PhaseStepGaussian
+
+
+### Optical Computing
+from .oc_field_2d import ContinuousFeatureGaussian

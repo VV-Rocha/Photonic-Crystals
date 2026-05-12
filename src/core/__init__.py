fca4d94f11563b594c22ae88591d381e5e49e30b
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-05-12 14:54:16 +0100

    Exposed the functions plot_2d and plot_3d to plot the 2d dimensional fields in a 2d and 3d figure, respectively. Exposed the PlotConfigMethods used for carrying the definitions and protocols to set the parameters for the plots.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -6,6 +6,10 @@ __all__ = [
     'AnalogousTime2DSimulationBox',
     'PhotorefractiveModel',
     'WavevectorPhotorefractiveModel',
+    # plotting
+    'plot_2d',
+    'plot_3d',
+    'PlotConfigMethods',
 ]
 
 from .mesh.z_2d import Mesh2D
@@ -23,3 +27,9 @@ from .engines.solvers.nls.solver_2d.solver import SplitStepSolver
 
 # storage
 from .storage.store_methods import StorageField
+
+
+# plotting
+from .plotting import plot_2d
+from .plotting import plot_3d
+from .plotting import PlotConfigMethods

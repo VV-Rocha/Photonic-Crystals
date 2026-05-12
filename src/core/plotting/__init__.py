eed8500f2f2b5b30bdfcdd82a94cc2d92f8c5e14
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-05-12 14:54:51 +0100

    Inside the plotting folder exposed the plot_2d and plot_3d as well as PlotConfigMethods.

---
+++ src/core/plotting/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'PlotConfigMethods',
+    'plot_2d',
+    'plot_3d',
+]
+
+
+from .plot_config.plot_config import PlotConfigMethods
+
+from .field_2d import plot_2d
+from .field_3d import plot_3d

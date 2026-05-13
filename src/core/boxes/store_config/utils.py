1334be8748d26580ca70b72623c408f2c22eb1fa
VV-Rocha <vicentevrocha.vvr@gmail.com>
2026-05-13 16:42:49 +0100

    removed deprecated config storing functions

--- src/core/boxes/store_config/utils.py
+++
@@ -1,9 +0,0 @@
-def _object_has_dict_attributes(value):
-    if not hasattr(value, "__dict__"):
-        return False
-
-    for attr_value in vars(value).values():
-        if isinstance(attr_value, dict):
-            return True
-
-    return False
